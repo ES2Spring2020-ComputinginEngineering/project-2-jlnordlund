1d2ffbd046362cdc24a26cfdf171a93caad96ec4
jlnordlund <60247191+jlnordlund@users.noreply.github.com>
2020-04-14 13:21:54 -0400

    statistics

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -32,3 +32,27 @@ for i in range(k):
     print('Centroid ', i+1, ' : ', int(centroid_g[i]), int(centroid_h[i]))
     
 kmc.graphingKMeans(glucose,hemoglobin,new_class,centroid_g,centroid_h)
+
+
+
+#statistics (only works when k = 1)
+num_true_p  = 0
+num_false_p = 0
+num_true_n  = 0
+num_false_n = 0
+for i in range(len(new_class)):
+    if new_class[i] == 0:
+        if classification[i] == 0:
+            num_true_n = num_true_n + 1
+        else:
+            num_false_n = num_false_n + 1
+    else:
+        if classification[i] == 0:
+            num_false_p = num_false_p + 1
+        else:
+            num_true_p = num_true_p + 1
+
+print('True Positive:',num_true_p/len(new_class))
+print('False Positive:',num_false_p/len(new_class))
+print('True Negative:',num_true_n/len(new_class))
+print('False Negative:',num_false_n/len(new_class))
